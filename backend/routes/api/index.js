0bf3f6b721ae579522cd61614220f02a929eda59
sunir <sunirkmanandhar@gmail.com>
2024-05-16 16:26:20 -0700

    Second Backend setup commit

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+
+router.use('/users', usersRouter);
+
 router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
 });
---
+++ backend/routes/api/users.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { jwtConfig } = require('../../config');
+const { User } = require('../../db/models');
+const router = express.Router();
+
+
+router.post('/register', async (req, res) => {
+    const { username, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ username, email, password: hashedPassword });
+    res.json({ user });
+});
+
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email }});
+
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+        return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    const token = jwt.sign({ id: user.id }, jwtConfig.secret, { expiresIn: jwtConfig.expiresIn });
+    res.json({ token });
+})
+
+module.exports = router;
